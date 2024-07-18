8710f19c8ed6b07031bd48c284d8ca9b9b0d0427
Ignacio Sanchez <sanchezih@gmail.com>
2024-07-18 10:43:50 -0300

    Update Punto3D.java

--- src/main/java/com/github/sanchezih/finalkeyword/_01_clase_final/Punto3D.java
+++ src/main/java/com/github/sanchezih/finalkeyword/_01_clase_final/Punto3D.java
@@ -1,6 +1,9 @@
 package com.github.sanchezih.finalkeyword._01_clase_final;
 
 /**
+ * Clase que intenta extender la clase Punto. Esto no sera posible porque la
+ * clase Punto fue declarada como final. Al intentar hacerlo, se obtendra un
+ * error en tiempo de compilacion.
  * 
  * @author ihsanch
  *
