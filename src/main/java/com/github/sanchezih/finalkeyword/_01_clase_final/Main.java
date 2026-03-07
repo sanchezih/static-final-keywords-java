caab3312defad00923ab078f67471d21b95d07ed
Ignacio Sanchez <sanchezih@gmail.com>
2026-03-07 10:42:33 -0300

    update

--- src/main/java/com/github/sanchezih/finalkeyword/_01_clase_final/Main.java
+++ src/main/java/com/github/sanchezih/finalkeyword/_01_clase_final/Main.java
@@ -2,13 +2,13 @@ package com.github.sanchezih.finalkeyword._01_clase_final;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Punto punto1 = new Punto(4, 5);
-        punto1.mostrarCuadrante();
+		Punto punto1 = new Punto(4, 5);
+		punto1.mostrarCuadrante();
 
-        Punto punto2 = new Punto(-4, 5);
-        punto2.mostrarCuadrante();
-    }
+		Punto punto2 = new Punto(-4, 5);
+		punto2.mostrarCuadrante();
+	}
 
 }
--- src/main/java/com/github/sanchezih/finalkeyword/_03_variable_final/Main.java
+++ src/main/java/com/github/sanchezih/finalkeyword/_03_variable_final/Main.java
@@ -1,15 +1,20 @@
 package com.github.sanchezih.finalkeyword._03_variable_final;
 
 /**
- * Cuando una variable se declara con la palabra clave final, su valor no se
- * puede modificar, esencialmente, es una constante.
- * Esto tambien significa que debe inicializar una variable final.
+ * Cuando una variable se declara con la palabra clave 'final', su valor solo
+ * puede asignarse una vez.
+ * 
+ * Despues de inicializarla, no puede modificarse.
  */
 public class Main {
 
-    public static void main(String[] args) {
-        final double PI = 3.14159;
-        System.out.println("Valor de PI: " + PI);
-    }
+	public static void main(String[] args) {
+
+		final double PI = 3.14159;
+
+		System.out.println("Valor de PI: " + PI);
+
+		// PI = 3.14; // Error de compilacion
+	}
 
 }
